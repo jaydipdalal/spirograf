da8915fa0b512c01f21de2e2b97b18ef7695e600
Jaydip Dalal <jaydip.dalal@gmail.com>
2020-06-27 23:33:11 -0700

    Add initial draw circle test

--- spirograf.js
+++ spirograf.js
@@ -4,35 +4,47 @@ function updateTextInput(val, type) {
 };
 
 
-// TEST draw pixel
+// // TEST draw pixel
 
-let canvas = document.getElementById("display-canvas");
-let canvasWidth = canvas.width;
-let canvasHeight = canvas.height;
-let ctx = canvas.getContext("2d");
-let canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+// let canvas = document.getElementById("display-canvas");
+// let canvasWidth = canvas.width;
+// let canvasHeight = canvas.height;
+// let ctx = canvas.getContext("2d");
+// let canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
 
-function drawPixel(x, y, r, g, b, a) {
-    let index = (x + y * canvasWidth) * 4;
-    canvasData.data[index + 0] = r;
-    canvasData.data[index + 1] = g;
-    canvasData.data[index + 2] = b;
-    canvasData.data[index + 3] = a;
-};
+// function drawPixel(x, y, r, g, b, a) {
+//     let index = (x + y * canvasWidth) * 4;
+//     canvasData.data[index + 0] = r;
+//     canvasData.data[index + 1] = g;
+//     canvasData.data[index + 2] = b;
+//     canvasData.data[index + 3] = a;
+// };
 
-function updateCanvas() {
-    ctx.putImageData(canvasData, 0, 0);
-};
+// function updateCanvas() {
+//     ctx.putImageData(canvasData, 0, 0);
+// };
 
-function draw_test() {
-    drawPixel(3, 3, 0, 255, 0, 255);
-    drawPixel(3, 4, 0, 255, 0, 255);
-    drawPixel(3, 5, 0, 255, 0, 255);
-    drawPixel(3, 6, 0, 255, 0, 255);
-    drawPixel(3, 7, 0, 255, 0, 255);
-    drawPixel(3, 8, 0, 255, 0, 255);
-    drawPixel(3, 9, 0, 255, 0, 255);
-    drawPixel(3, 10, 0, 255, 0, 255);
-    drawPixel(3, 11, 0, 255, 0, 255);
-    updateCanvas();
+// function draw_test() {
+//     drawPixel(3, 3, 0, 255, 0, 255);
+//     drawPixel(3, 4, 0, 255, 0, 255);
+//     drawPixel(3, 5, 0, 255, 0, 255);
+//     drawPixel(3, 6, 0, 255, 0, 255);
+//     drawPixel(3, 7, 0, 255, 0, 255);
+//     drawPixel(3, 8, 0, 255, 0, 255);
+//     drawPixel(3, 9, 0, 255, 0, 255);
+//     drawPixel(3, 10, 0, 255, 0, 255);
+//     drawPixel(3, 11, 0, 255, 0, 255);
+//     updateCanvas();
+// };
+
+
+// TEST draw initial circle
+
+function reset_drawing() {
+    var canvas = document.getElementById("display-canvas");
+    var ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(450, 450, 150, 0, 2 * Math.PI);
+    // ctx.strokeStyle = "#00FF00";
+    ctx.stroke();
 };
