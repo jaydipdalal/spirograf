15e7fd5170680ce90aae508be7a090ad8cc48f91
Jaydip Dalal <jaydip.dalal@gmail.com>
2020-07-02 23:16:28 -0700

    Clean up js functions

--- spirograf.js
+++ spirograf.js
@@ -6,7 +6,7 @@ let elementCol = "#283654";
 let elementedSelectedCol = "#4D638C";
 let textCol = "#CFE0F9";
 
-// Global canvas
+// Global canvases
 let canvasBase = document.getElementById("display-canvas-base");
 let contextBase = canvasBase.getContext("2d");
 let canvasDrawing = document.getElementById("display-canvas-drawing");
@@ -16,9 +16,7 @@ let contextDrawing = canvasDrawing.getContext("2d");
 let baseCircle = { radius: 300, radiusMin: 150, radiusMax: 300, cx: canvasBase.width / 2, cy: canvasBase.height / 2 };
 
 let rotatorCircle = { radius: 99, radiusMin: 30, radiusMax: 150 };
-function updateRotatorCircle() {
-    rotatorCircle = { ...rotatorCircle, ...{ cx: baseCircle.cx + baseCircle.radius - rotatorCircle.radius, cy: baseCircle.cy } };
-};
+updateRotatorCircle = () => rotatorCircle = { ...rotatorCircle, ...{ cx: baseCircle.cx + baseCircle.radius - rotatorCircle.radius, cy: baseCircle.cy } };
 updateRotatorCircle();
 
 // Global drawing point
@@ -29,9 +27,8 @@ let time = 0;
 let running = false;
 
 
-// TEST draw initial circles
-
-function initDrawing() {
+// Draw initial circles
+initDrawing = () => {
     time = 0;
 
     contextBase.clearRect(0, 0, canvasBase.width, canvasBase.height);
@@ -50,12 +47,10 @@ function initDrawing() {
     contextDrawing.beginPath();
     contextDrawing.translate(canvasBase.width / 2, canvasBase.height / 2);
 };
-initDrawing();
 
 
-// TEST draw rotating circle and cooresponding point
-
-function startDrawing() {
+// Draw rotating circle and corrresponding point
+startDrawing = () => {
     if (running) {
         let rotatorAngle = (((2 * Math.PI) / speed.time) * time++);
         let rotatingPointX = rotatingPoint.distance * Math.cos((baseCircle.radius - rotatorCircle.radius) / rotatorCircle.radius * rotatorAngle);
@@ -86,18 +81,18 @@ function startDrawing() {
 
 // CONTROL functions
 
-function start() {
+start = () => {
     if (!running) {
         running = true;
         window.requestAnimationFrame(startDrawing);
     };
 };
 
-function stop() {
+stop = () => {
     running = false;
 };
 
-function reset() {
+reset = () => {
     stop();
     updateTextInput(300, 'options-content-value-1');
     updateTextInput(99, 'options-content-value-2');
@@ -107,9 +102,9 @@ function reset() {
 };
 
 
-// TEST variable parameters
+// TEST update variable parameters
 
-function updateTextInput(val, type) {
+updateTextInput = (val, type) => {
     let valNum = parseInt(val);
     
     switch(type) {
@@ -153,22 +148,21 @@ function updateTextInput(val, type) {
     initDrawing();
 };
 
-function updateColor(newColor) {
+updateColor = (newColor) => {
     color = newColor;
     document.getElementsByClassName("options-content-input-element-inline-1")[0].value = newColor;
 };
 
 
-// TEST download png
-
-function downloadPNG() {
+// Download PNG of drawing
+downloadPNG = () => {
     document.getElementById("downloader").href = document.getElementById("display-canvas-drawing").toDataURL("image/png");
 };
 
 
 // HELPERS canvas drawers
 
-function drawArc(context, cx, cy, radius, fillCol = 0, strokeCol = 0) {
+drawArc = (context, cx, cy, radius, fillCol = 0, strokeCol = 0) => {
     context.beginPath();
     context.arc(cx, cy, radius, 0, 2 * Math.PI);
     context.fillStyle = fillCol;
@@ -177,10 +171,14 @@ function drawArc(context, cx, cy, radius, fillCol = 0, strokeCol = 0) {
     context.stroke();
 };
 
-function drawLine(context, toggleBegin = 0, srcX = false, srcY = false, destX, destY, strokeCol = 0) {
+drawLine = (context, toggleBegin = 0, srcX = false, srcY = false, destX, destY, strokeCol = 0) => {
     if (toggleBegin !== 0) { context.beginPath(); };
     if (srcX !== false) { context.moveTo(srcX, srcY); };
     context.lineTo(destX, destY);
     if (strokeCol !== 0) { context.strokeStyle = strokeCol; };
     context.stroke();
 };
+
+
+// Initialize app
+initDrawing();
